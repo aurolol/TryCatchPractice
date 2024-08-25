0c1c492fd79eb48cbe22991db7d8893f95249c1e
auro <auro@DESKTOP-AV8I9BT>
2024-08-25 23:55:44 +0300

    Add project files.

---
+++ TryCatchPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿
+try 
+{
+    Console.Write("Bir sayı giriniz.");
+
+    int sayi = Convert.ToInt32(Console.ReadLine());
+
+    int karesi = sayi * sayi;
+
+    Console.WriteLine(karesi);
+
+}
+catch(FormatException)
+{
+    Console.WriteLine("Yanlış format girdiniz." );
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Beklenmeyen bir hata oluştu." + ex.Message);
+}
